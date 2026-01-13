d1d462d128edde1b5147893cc3bb1f0cfb72eaf8
NiiDk <darkosammy2@gmail.com>
2026-01-13 17:14:11 +0000

    Update urls.py

--- sellux_plaster/urls.py
+++ sellux_plaster/urls.py
@@ -3,12 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView  # Added for robots.txt
 from .sitemaps import StaticViewSitemap
 
-# Custom Branding for Django Administration
+# Custom Branding
 admin.site.site_header = "Sellux Plaster Administration"
 admin.site.site_title = "Sellux Plaster Admin Portal"
 admin.site.index_title = "Welcome to Sellux Plaster Business Manager"
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,7 +21,7 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('services/', include('services.urls')),
     path('portfolio/', include('portfolio.urls')),
-    path('shop/', include('catalogue.urls')), # Renamed from 'catalogue'
+    path('shop/', include('catalogue.urls')), 
     path('faq/', include('faq.urls')),
     path('contact/', include('contact.urls')),
     path('orders/', include('orders.urls')),
@@ -28,17 +33,12 @@ urlpatterns = [
     path('promotions/', include('promotions.urls')),
     path('about/', include('about.urls')),
     path('team/', include('team.urls')),
+    
+    # Sitemap and Robots.txt (Corrected Integration)
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-sitemaps = {
-    'static': StaticViewSitemap,
-}
-
-urlpatterns = [
-    ...
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-]
